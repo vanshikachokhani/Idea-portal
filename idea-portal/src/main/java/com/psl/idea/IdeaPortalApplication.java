7dde06396169f919058154678d941f74d6a350c6
Vanshika <vanshikadeepakchokhani@gmail.com>
2021-07-13 17:36:01 +0530

    Basic project structure

---
+++ idea-portal/src/main/java/com/psl/idea/IdeaPortalApplication.java
@@ -0,0 +1,13 @@
+package com.psl.idea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeaPortalApplication.class, args);
+	}
+
+}
---
+++ idea-portal/src/test/java/com/psl/idea/IdeaPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psl.idea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
