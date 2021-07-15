80734268c5f6c37c17964b6db7ec554162015daa
Bharat <chaudharyb141@gmail.com>
2021-07-15 20:35:21 +0530

    Revert "Added RatingId in Rating class and implemented RatingId"
    This reverts commit c226cbe097b814427e29444e554f12294c0b6b89.

--- idea-portal/src/main/java/com/psl/idea/models/Rating.java
+++ idea-portal/src/main/java/com/psl/idea/models/Rating.java
@@ -4,14 +4,12 @@ import java.io.Serializable;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.IdClass;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="ratings")
-@IdClass(RatingId.class)
 public class Rating implements Serializable {
 
 	private static final long serialVersionUID = 1L;
