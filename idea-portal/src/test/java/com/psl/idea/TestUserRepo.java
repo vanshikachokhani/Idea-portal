2dc6ff440d81ae19599cdb30b13bb361283104f8
Vaibhav <vaibhavsingh.ece18@gmail.com>
2021-07-22 20:16:00 +0530

    Updated testuserrepo

--- idea-portal/src/test/java/com/psl/idea/TestUserRepo.java
+++ idea-portal/src/test/java/com/psl/idea/TestUserRepo.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import com.psl.idea.models.Privilege;
 import com.psl.idea.models.Users;
@@ -11,7 +12,7 @@ import com.psl.idea.repository.UserRepo;
 
 class TestUserRepo {
 
-	@Autowired
+	@MockBean
 	UserRepo userRepo;
 
 	@Autowired
@@ -23,6 +24,7 @@ class TestUserRepo {
 		p.setPrivilege("ProductManager");
 		p.setPrivilegeId(1);
 		Users user=new Users("vaibhav","8423","email@gmail.com","pass",p);
+		System.out.println(user.toString());
 		assertEquals(user, userRepo.findByEmailId("email@gmail.com"));
 	}
 
