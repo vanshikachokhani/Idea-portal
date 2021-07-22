90a60b35b13797f185335a5ec8ec2c4435d8e365
Bharat <chaudharyb141@gmail.com>
2021-07-22 19:47:47 +0530

    Added UNAUTHORIZED 401 Exception Handler

--- idea-portal/src/main/java/com/psl/idea/exceptions/CustomExceptionHandler.java
+++ idea-portal/src/main/java/com/psl/idea/exceptions/CustomExceptionHandler.java
@@ -36,5 +36,17 @@ public class CustomExceptionHandler {
 		error.setMessage(e.getMessage());
 		return new ResponseEntity<String>(error.toString(), HttpStatus.INTERNAL_SERVER_ERROR);
 	}
+	
+	@ExceptionHandler(UnauthorizedException.class)
+	public ResponseEntity<String> NotAuthorizedExceptionHandler (UnauthorizedException ex){
+		CustomErrorMessage error = new CustomErrorMessage();
+		error.setStatus(401);
+		error.setError("UNAUTHORIZED");
+		error.setMessage(ex.getMessage());
+
+	        return new ResponseEntity<String>(error.toString(), HttpStatus.UNAUTHORIZED);
+		
+		
+	}
 
 }
