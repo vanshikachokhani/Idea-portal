2c3863e98490d3e95162153535d0b4cd4914c4f0
RohanIRathi <rathirohan8@gmail.com>
2021-07-23 17:45:05 +0530

    Added IdeaServiceTest

---
+++ idea-portal/src/test/java/com/psl/idea/service/IdeaServiceTest.java
@@ -0,0 +1,63 @@
+package com.psl.idea.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.psl.idea.models.Category;
+import com.psl.idea.models.Idea;
+import com.psl.idea.models.Privilege;
+import com.psl.idea.models.Theme;
+import com.psl.idea.models.Users;
+import com.psl.idea.repository.IdeaRepo;
+import com.psl.idea.repository.ThemeRepo;
+
+@WebMvcTest(controllers=IdeaService.class)
+@ExtendWith(SpringExtension.class)
+public class IdeaServiceTest {
+	
+	@MockBean
+	ThemeRepo themeRepo;
+	@MockBean
+	IdeaRepo ideaRepo;
+	
+	@Autowired
+	IdeaService ideaService;
+	@Autowired
+	MockMvc mockMvc;
+	
+	String[] files = {};
+	Privilege cpPrivilege = new Privilege(1, "Client Partner");
+	Category category = new Category(1, "WebApp");
+	Users user = new Users(1, "Rohan Rathi", "8830850720", "rohan_rathi@persistent.com", "RohanRathi", "Persistent", cpPrivilege);
+	Theme theme = new Theme(1, "Test Theme", "Testing Theme", category, user, files);
+	Idea idea = new Idea(1, "Test Idea", "Testing Theme", files, 0, theme, user);
+	
+	@Test
+	public void createIdeaServiceTest() {
+		Optional<Theme> optionalTheme = Optional.of(theme);
+		when(themeRepo.findById(theme.getThemeId())).thenReturn(optionalTheme);
+		when(ideaRepo.save(idea)).thenReturn(idea);
+		
+		assertEquals(idea, ideaService.createIdea(1, idea));
+		
+		optionalTheme = Optional.empty();
+		when(themeRepo.findById((long) 2)).thenReturn(optionalTheme);
+		
+		assertEquals(null, ideaService.createIdea(2, idea));
+		
+		verify(themeRepo).findById(theme.getThemeId());
+		verify(ideaRepo).save(idea);
+	}
+
+}
