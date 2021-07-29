152106905523fa6219557977c64d5385f5dd7c5a
RohanIRathi <rathirohan8@gmail.com>
2021-07-29 16:32:15 +0530

    Added packaging for jar

--- idea-portal/src/main/java/com/psl/idea/IdeaPortalApplication.java
+++ idea-portal/src/main/java/com/psl/idea/IdeaPortalApplication.java
@@ -32,7 +32,11 @@ public class IdeaPortalApplication extends SpringBootServletInitializer{
 
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/api/**").allowedOrigins("http://localhost:4200", "http://localhost:8080", "https://hopeful-leavitt-8a8a98.netlify.app");
+				registry.addMapping("/api/**").allowedOrigins("http://localhost:4200",
+						"http://localhost:8080",
+						"https://hopeful-leavitt-8a8a98.netlify.app",
+						"https://psl-ideaportal.herokuapps.com"
+						);
 			}
 		};
 	}
