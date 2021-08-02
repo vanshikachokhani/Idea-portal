fc8e9fa8f906659407d28791a2dc0c436e0d33c3
RohanIRathi <rathirohan8@gmail.com>
2021-08-02 13:10:20 +0530

    Fixed view ideas by comments

--- idea-portal/src/main/java/com/psl/idea/repository/IdeaRepo.java
+++ idea-portal/src/main/java/com/psl/idea/repository/IdeaRepo.java
@@ -13,7 +13,10 @@ public interface IdeaRepo extends JpaRepository<Idea, Long> {
 	Idea findByIdeaId(long idea_id);
 	Idea[] findAllByThemeThemeId(long themeId);
 	
-	@Query(value ="FROM Idea I where I.theme.themeId=?1 LEFT OUTER JOIN FROM Comment C WHERE C.theme.themeId=?1 GROUP BY C.idea.ideaId ON I.theme=C.theme ORDER BY COUNT(C.commentId) DESC",nativeQuery = true)
+	@Query(value ="SELECT *\r\n"
+			+ "	FROM ideaportal.ideas I\r\n"
+			+ "	LEFT OUTER JOIN ideaportal.comments C ON I.idea_id=C.idea_id WHERE theme_id=?1\r\n"
+			+ "	GROUP BY I.idea_id ORDER BY COUNT(C.idea_id) DESC;",nativeQuery = true)
 	List<Idea> findbycomment(long themeID);
 
 }
