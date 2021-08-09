a2019c42f333f0d46a9f04d8d577b7426447c7f0
Bharat <chaudharyb141@gmail.com>
2021-08-09 10:11:30 +0530

    Float Rating Added

--- idea-portal/src/main/java/com/psl/idea/repository/RatingRepo.java
+++ idea-portal/src/main/java/com/psl/idea/repository/RatingRepo.java
@@ -3,6 +3,7 @@ package com.psl.idea.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.psl.idea.models.Rating;
 import com.psl.idea.models.RatingId;
@@ -10,5 +11,11 @@ import com.psl.idea.models.RatingId;
 public interface RatingRepo extends JpaRepository <Rating, RatingId> {
 	
 	public List<Rating> findByIdeaIdeaId(long idea_id);
+	
+	@Query("SELECT COALESCE(sum(CASE WHEN rating THEN 1 ELSE 0 END),0) FROM ratings")
+	int findTruevalue();
+	
+	@Query("SELECT COUNT(*) FROM ratings")
+	int findTotal();
 
 }
