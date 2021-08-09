c900c6a117e8b8c82e4c0481c25fbcbf6d81a24c
Vaibhav <vaibhavsingh.ece18@gmail.com>
2021-08-09 12:48:43 +0530

    Added test class template for IdeaRepoImpl

---
+++ idea-portal/src/test/java/com/psl/idea/TestIdeaRepoImpl.java
@@ -0,0 +1,40 @@
+package com.psl.idea;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestIdeaRepoImpl {
+
+	@Test
+	public void testfindbyUserUserId() {
+		
+		
+	}
+
+	@Test
+	public void testfindbylike() {
+		
+		
+	}
+	
+	@Test
+	public void testfindbydate() {
+		
+		
+	}
+	
+	@Test
+	public void testfindbycomment() {
+		
+		
+	}
+	
+	@Test
+	public void testfindAll() {
+		
+		
+	}
+	
+	
+}
